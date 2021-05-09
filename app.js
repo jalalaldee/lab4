62c375c98dc323e56dc817fc5d492a242512dcd7
jalalaldeen <>
2021-05-09 22:53:10 +0000

    df

---
+++ app.js
@@ -0,0 +1,22 @@
+alert('wellcom word');
+
+var age =prompt('how old are you?');
+
+if (age >= 10 && age <= 80) {
+  alert('Welcome to my site');
+} else if (age < 10 || age >= 80) {
+  alert('Sorry you are not allowed')
+} else {
+  alert('Have a nice day!');
+}
+
+
+var wisdomCount = prompt('How many wisdom do you want to read ?')
+
+
+
+
+for (var wisdomCount = 0; x < wisdomCount ;x++){
+   document.write('https://www.anacereddine.com/assets/images/Words_of_wisdom_English__2_.jpg')
+}
+ 
